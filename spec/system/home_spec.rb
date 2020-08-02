2c447b2f8759196efaaf6b73408526a6c452a0ab
estsn122 <y.tani9411@gmail.com>
2020-08-02 19:15:57 +0900

    [Add]本ブランチで実装する箇所を縮小

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 RSpec.describe 'トップ画面', type: :system do
   it 'タイトル表示がある' do
     visit root_path
-    expect(page).to have_content('test')
-  end
-  it 'ログインボタンがある' do
-    visit root_path
-    expect(page).to have_content('test')
+    expect(page).to have_content('リプアニ')
   end
 end
 
