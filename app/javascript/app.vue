e6d72a55ea5254a45af813273372d42600dc850a
estsn122 <y.tani9411@gmail.com>
2020-08-05 20:46:29 +0900

    [Add]ヘッダーを作成

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,7 +1,18 @@
 <template>
   <v-app>
     <div>
+      <TheHeader />
       <router-view />
     </div>
   </v-app>
 </template>
+
+<script>
+import TheHeader from './components/TheHeader'
+
+export default {
+  components: {
+    TheHeader,
+  }
+}
+</script>
---
+++ app/javascript/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dark app>
+      <v-toolbar-title>リプアニ</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default({
+  name: 'TheHeader',
+  data() {
+    return {
+    }
+  },
+})
+</script>
