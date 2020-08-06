10aabfea4bc4e362710497f485f00702a5c2a16b
estsn122 <y.tani9411@gmail.com>
2020-08-06 22:54:41 +0900

    [Add]Animeモデルの定義を変更

--- db/migrate/20200805131730_create_animes.rb
+++ db/migrate/20200805131730_create_animes.rb
@@ -2,11 +2,13 @@ class CreateAnimes < ActiveRecord::Migration[6.0]
   def change
     create_table :animes do |t|
       t.string :title, null: false
-      t.string :title_short
+      t.string :title_short1
+      t.string :title_short2
+      t.string :title_short3
       t.string :public_url
       t.string :twitter_account
       t.string :twitter_hash_tag
-      t.integer :target_sex
+      t.integer :sex
       t.integer :sequel
       t.timestamps
     end
