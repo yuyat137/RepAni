78326a372cf0b7d15ae15b6fc952107e64bfb962
estsn122 <y.tani9411@gmail.com>
2020-08-06 23:07:18 +0900

    [Fix]Animeモデルの変更に伴いモデルメソッドを修正

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -9,23 +9,9 @@ class Anime < ApplicationRecord
     return if response.code != "200"
 
     results = JSON.parse(response.body, symbolize_names: :true)
-    results.map!{ |anime| anime.slice(:title, :title_short1, :public_url, :twitter_account, :twitter_hash_tag, :sex, :sequel)}
+    results.map!{ |anime| anime.slice(:title, :public_url, :twitter_account, :twitter_hash_tag, :sex, :sequel)}
     return if results.blank?
 
-    # TODO: リファクタリング対応
-    results.each do |result|
-      tmp1 = result[:title_short1]
-      result[:title_short] = tmp1
-      result.delete(:title_short1)
-      tmp2 = result[:sex]
-      result[:target_sex] = tmp2
-      result.delete(:sex)
-    end
-
-    animes = []
-    results.each do |result|
-      animes << Anime.new(result)
-    end
-    Anime.import animes, validate: true
+    Anime.import results, validate: true
   end
 end
