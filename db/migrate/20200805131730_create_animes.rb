58983d0128db3f787f26da7339ab1e0191bb7c3a
estsn122 <y.tani9411@gmail.com>
2020-08-06 22:58:22 +0900

    [Fix]Animeモデルの定義を再度変更

--- db/migrate/20200805131730_create_animes.rb
+++ db/migrate/20200805131730_create_animes.rb
@@ -2,9 +2,6 @@ class CreateAnimes < ActiveRecord::Migration[6.0]
   def change
     create_table :animes do |t|
       t.string :title, null: false
-      t.string :title_short1
-      t.string :title_short2
-      t.string :title_short3
       t.string :public_url
       t.string :twitter_account
       t.string :twitter_hash_tag
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_131730) do
 
   create_table "animes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
-    t.string "title_short1"
-    t.string "title_short2"
-    t.string "title_short3"
     t.string "public_url"
     t.string "twitter_account"
     t.string "twitter_hash_tag"
