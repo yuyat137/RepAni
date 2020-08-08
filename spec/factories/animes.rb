1ab182ebb5ac82d7cc8310affc6211803a30bfcb
estsn122 <y.tani9411@gmail.com>
2020-08-08 12:21:33 +0900

    [Add]アニメのindexメソッドのリクエストスペックを作成

--- spec/factories/animes.rb
+++ spec/factories/animes.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :anime do
-    
+    sequence(:title) { |n| "title_#{n}" }
+    broadcast_datetime { DateTime.now }
+    air_time { 30 }
+    public_url { 'http://example.com' }
+    sequence(:twitter_account) { |n| "test_screen_name_#{n}" }
+    sequence(:twitter_hash_tag) { |n| "test_hash_tag_#{n}" }
   end
 end
