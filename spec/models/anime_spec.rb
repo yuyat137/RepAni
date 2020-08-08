6564f316e1f368cbcb472bf12cb315513c067e91
estsn122 <y.tani9411@gmail.com>
2020-08-08 13:47:10 +0900

    [Add]アニメのモデルスペックを追加

--- spec/models/anime_spec.rb
+++ spec/models/anime_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Anime, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タイトルがないと無効' do
+    anime = build(:anime, title:'')
+    anime.valid?
+    expect(anime.errors[:title]).to include('を入力してください')
+  end
 end
