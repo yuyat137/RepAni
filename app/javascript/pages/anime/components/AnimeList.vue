3ffe2b01ec3437e5e478b65e57d99aa47e5d489e
estsn122 <y.tani9411@gmail.com>
2020-08-10 14:09:51 +0900

    [Add]component化

---
+++ app/javascript/pages/anime/components/AnimeList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-container class="grey lighten-5">
+      <v-row>
+        <v-col
+          v-for="anime in animes"
+          :key="anime.id"
+          cols="4"
+        >
+          <v-card
+            class=""
+            outlined
+            @click="handleSelectAnime(anime)"
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">
+                  OVERLINE
+                </div>
+                <v-list-item-title class="headline mb-1">
+                  {{ anime.title }}
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-card-actions />
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnimeList",
+  props: {
+    animes: {
+      type: Array,
+      required: true
+    },
+  },
+  methods: {
+    handleSelectAnime(anime) {
+      this.$emit('select-anime', anime)
+    }
+  },
+}
+</script>
--- app/javascript/pages/anime/index.vue
+++ app/javascript/pages/anime/index.vue
@@ -4,66 +4,29 @@
       Topへ
     </router-link>
     <h2>{{ selectTerm.year }}年{{ selectTerm.season_ja }}</h2>
-    <v-container class="grey lighten-5">
-      <v-row>
-        <v-col
-          v-for="anime in animes"
-          :key="anime.id"
-          cols="4"
-        >
-          <v-card
-            class=""
-            outlined
-            @click="handleStartTweetReplay(anime)"
-          >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="overline mb-4">
-                  OVERLINE
-                </div>
-                <v-list-item-title class="headline mb-1">
-                  {{ anime.title }}
-                </v-list-item-title>
-              </v-list-item-content>
-              <v-card-actions />
-            </v-list-item>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <AnimeList
+      :animes="animes"
+      @select-anime="handleStartTweetReplay"
+    />
     <h2>シーズン一覧</h2>
-    <v-container class="grey lighten-5">
-      <v-row>
-        <v-col
-          v-for="term in terms"
-          :key="term.id"
-          cols="3"
-        >
-          <v-card
-            class="rounded-xl"
-            outlined
-            @click="handleShowSelectTerm(term)"
-          >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <v-list-item-title class="headline mb-1">
-                  {{ term.year }}年{{ term.season_ja }}アニメ
-                </v-list-item-title>
-              </v-list-item-content>
-              <v-card-actions />
-            </v-list-item>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <TermList
+      :terms="terms"
+      @select-term="handleShowSelectTerm"
+    />
   </div>
 </template>
 
 <script>
 /* import Mixin from '../../packs/mixins/mixin' */
+import AnimeList from './components/AnimeList'
+import TermList from './components/TermList'
 
 export default {
   name: "AnimeIndex",
+  components: {
+    AnimeList,
+    TermList,
+  },
   data() {
     return {
       animes: [],
@@ -97,6 +60,8 @@ export default {
       }
     },
     handleShowSelectTerm(term) {
+      console.log("term")
+      console.log(term)
       this.selectTerm = term;
       this.$axios.get("animes", { params: term })
         .then(res => this.animes = res.data)
