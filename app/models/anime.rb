ef1f734334ce2d690f4d983ea6bc825c5d56d7c3
estsn122 <y.tani9411@gmail.com>
2020-08-22 14:20:04 +0900

    [Fi]アニメをインポートするメソッドを修正

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,27 +1,52 @@
 class Anime < ApplicationRecord
+  before_validation :set_state
   has_many :anime_terms
   has_many :terms, through: :anime_terms
   has_many :episodes
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :state, presence: true
   enum state: %i[close open]
+  RESPONSE_SUCCESS = '200'.freeze
+  SHANGRILA_API_URI = 'http://api.moemoe.tokyo/anime/v1/master/'.freeze
 
-  def self.import_by_api(year = nil, season = nil)
-    term = Term.get(year, season)
-    url = 'http://api.moemoe.tokyo/anime/v1/master/' + term.year.to_s + '/' + term.season_before_type_cast.to_s
-    response = Net::HTTP.get_response URI.parse(url)
-    return if response.code != '200'
+  # TODO: サービスオブジェクトにするか検討
+  def self.import_this_term(year = nil, season = nil)
+    term = (year && season)? Term.find_or_create_by(year: year, season: season) : Term.now
+
+    api_end_point = SHANGRILA_API_URI + term.year.to_s + '/' + term.season_before_type_cast.to_s
+    response = Net::HTTP.get_response URI.parse(api_end_point)
+    return if response.code != RESPONSE_SUCCESS
 
     json = JSON.parse(response.body, symbolize_names: true)
-    # TODO: 以下の箇所、リファクタリングする余地あり。サービスオブジェクト？
     json.each do |anime|
       anime.slice!(:title, :public_url, :twitter_account, :twitter_hash_tag)
-      created_anime = Anime.new(anime)
-      next unless created_anime.valid?
+      new_anime = Anime.new(anime)
+      next unless new_anime.valid?
 
-      created_anime.save
-      created_anime.anime_terms.create(term: term)
+      new_anime.save
+      new_anime.anime_terms.create(term: term)
     end
-    Term.set_now
+    Term.update_now
+  end
+
+  def set_episodes(episode_num)
+      # new_tweets << Tweet.new(
+      #   tweet_id: tweet[:id],
+      #   name: tweet.dig(:user, :name),
+      #   screen_name: tweet.dig(:user, :screen_name),
+      #   text: tweet[:text],
+      #   image_url1: tweet.dig(:extended_entities, :media, 0, :media_url_https),
+      #   image_url2: tweet.dig(:extended_entities, :media, 1, :media_url_https),
+      #   image_url3: tweet.dig(:extended_entities, :media, 2, :media_url_https),
+      #   image_url4: tweet.dig(:extended_entities, :media, 3, :media_url_https),
+      #   tweeted_at: tweet[:created_at].in_time_zone('Tokyo')
+      # )
+    Tweet.import new_tweets, on_duplicate_key_ignore: true
+  end
+
+  private
+
+  def set_state
+    self.state = :open if state.nil?
   end
 end
