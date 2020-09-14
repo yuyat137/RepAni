5a5018b50a5365a3cee5f5be5e3bbaaa757aaed5
estsn122 <y.tani9411@gmail.com>
2020-09-14 23:49:38 +0900

    [Modify]ツイートの取得開始時間を(ある程度)きっちりするよう変更

--- app/services/search_tweets_service.rb
+++ app/services/search_tweets_service.rb
@@ -3,7 +3,8 @@ class SearchTweetsService
 
   # NOTE: Episode#import_associate_tweetsから実行されること想定
   # NOTE: ここで使用するmax_tweet_idは自力で取ってくることとします
-  #       検索例『#rezero until:2020-08-12_23:59:59_JST』
+  #       検索例『#rezero until:2020-09-09_23:59:59_JST』
+  #       検索例『#rezero until:2020-09-10_00:00:00_JST』
   #       https://twitter.com/millef_168/status/1293562877488427009
   #       id=>1293562877488427009
   def self.fetch_tweets(hashtag, max_tweet_id, air_time_min)
@@ -14,7 +15,8 @@ class SearchTweetsService
     hashtag = '#' + hashtag unless hashtag.include?('#')
     twitter = twitter_rest_client
 
-    fetch_tweets_at_anime_broadcast(twitter, hashtag, max_tweet_id, air_time_min)
+    fetched_tweets = fetch_tweets_at_anime_broadcast(twitter, hashtag, max_tweet_id, air_time_min)
+    extract_tweets(fetched_tweets, air_time_min)
   end
 
   private
@@ -41,5 +43,14 @@ class SearchTweetsService
 
   def calculate_diff_tweeted_sec(fetch_tweets)
     (Time.parse(fetch_tweets.first[:created_at]) - Time.parse(fetch_tweets.last[:created_at])).abs
+  end
+
+  def extract_tweets(fetched_tweets, air_time_min)
+    reference_time = DateTime.parse(fetched_tweets.last[:created_at]) - Rational(air_time_min, 24 * 60)
+    extracted_tweets = []
+    fetched_tweets.each do |tweet|
+      extracted_tweets << tweet if reference_time < DateTime.parse(tweet[:created_at])
+    end
+    extracted_tweets
   end
 end
