c09ee20fa63fbd483584fdff94232a45bb93d12d
estsn122 <y.tani9411@gmail.com>
2020-09-21 14:28:07 +0900

    [Fix]前ブランチまでのRspecが失敗してたので修正

--- spec/requests/tweets_request_spec.rb
+++ spec/requests/tweets_request_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe "Api::Tweets", type: :request do
   let!(:anime) { create(:anime, :associate_term, :episodes) }
   let!(:episode) { anime.episodes.first }
-  let!(:tweets) { create_list(:tweet, 20, episode_id: episode.id) }
+  let!(:tweets) { 20.times.collect { |i| create(:tweet, episode_id: episode.id, serial_number: i + 1) } }
+
   it 'index' do
-    get api_tweets_path, params: { episode_id: episode.id }
+    get api_tweets_path, params: { episode_id: episode.id, progress_time_msec: 0 }
     json = JSON.parse(response.body)
     expect(response).to have_http_status(200)
-    expect(json.length).to eq(tweets.length)
-    expect(json.first['text']).to eq(tweets.first.text)
+    expect(json["tweets"].length).to eq(tweets.length)
+    expect(json["tweets"].first['text']).to eq(tweets.first.text)
   end
 end
--- spec/system/replay_spec.rb
+++ spec/system/replay_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 RSpec.describe 'Replay', type: :system do
   let!(:anime) { create(:anime, :associate_now_term, :episodes) }
   let!(:episode) { anime.episodes.first }
-  let!(:tweets) { create_list(:tweet, 20, episode_id: episode.id) }
+  let!(:tweet1) { create(:tweet, episode_id: episode.id, serial_number: 1) }
+  let!(:tweet2) { create(:tweet, episode_id: episode.id, serial_number: 2) }
+
   describe 'タイマー' do
     context 'ページを開いたら' do
       it 'タイマーの初期表示がある' do
@@ -27,11 +29,12 @@ RSpec.describe 'Replay', type: :system do
         expect(page).to have_content("#{episode.num}話")
       end
       it 'ツイートが表示される' do
-        # TODO: このコードは本番とは動きが異なる。
         visit "/replay/#{episode.id}"
         find("#timer_start").click
-        expect(page).to have_content(tweets[0].text)
-        expect(page).to have_content(tweets[1].text)
+        sleep(1)
+        expect(page).to have_content(tweet1.text)
+        sleep(1)
+        expect(page).to have_content(tweet2.text)
       end
     end
   end
