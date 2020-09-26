2a6027d0eba71e855caf036b468cfeb0ba5bd223
estsn122 <y.tani9411@gmail.com>
2020-09-26 18:43:41 +0900

    [Add]ツイート表示をある程度規約に従った表示になるよう実装

---
+++ app/javascript/pages/replay/components/Tweet.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <v-card class="pa-2">
+      <div>
+        <div class="d-inline-block">
+          <img :src="tweet.profile_image_url" class="rounded-circle">
+        </div>
+        <div class="d-inline-block">
+          <div class="block">@{{ tweet.screen_name }}</div>
+          <div class="block" style="white-space: nowrap">{{ tweet.name }}</div>
+        </div>
+      </div>
+      <p style="white-space: pre-line">
+        {{ tweet.text }}
+      </p>
+      <div>
+        <img
+          v-if="tweet.image_url1"
+          :src="tweet.image_url1"
+          class="d-inline-block"
+          width="40%"
+        >
+        <img
+          v-if="tweet.image_url2"
+          :src="tweet.image_url2"
+          class="d-inline-block"
+          width="40%"
+        >
+        <img
+          v-if="tweet.image_url3"
+          :src="tweet.image_url3"
+          class="d-inline-block"
+          width="40%"
+        >
+        <img
+          v-if="tweet.image_url4"
+          :src="tweet.image_url4"
+          class="d-inline-block"
+          width="40%"
+        >
+      </div>
+      <div class="d-flex justify-space-between">
+        <div>
+          <img
+            src="../../../assets/reply.png"
+            class="ml-2"
+            width = "14px"
+            height = "12px"
+          />
+          <img
+            src="../../../assets/retweet.png"
+            class="ml-15"
+            width = "17px"
+            height = "11px"
+          />
+          <img
+            src="../../../assets/favorite.png"
+            class="ml-15"
+            width = "15px"
+            height = "12px"
+          />
+        </div>
+        <div>
+          {{ tweeted_at() }}
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script>
+import moment from 'moment';
+require("moment-duration-format");
+
+export default {
+  name: "Tweet",
+  props: {
+    tweet: {
+      type: Object,
+      required: true
+    },
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    tweeted_at() {
+      return String(moment(new Date(this.tweet.tweeted_at)).format("YYYY/MM/DD HH:mm:ss"))
+    }
+  }
+}
+</script>
