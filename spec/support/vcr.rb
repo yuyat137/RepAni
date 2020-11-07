540a365400006ada96e6b8398cf201511a6913f2
estsn122 <y.tani9411@gmail.com>
2020-11-07 20:29:39 +0900

    [Fix]VCRの設定を変更

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,6 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
 end
 
