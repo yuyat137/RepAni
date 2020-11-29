88a5efa66ab5e6c4735f7305041f873863cc4e74
estsn122 <y.tani9411@gmail.com>
2020-11-29 15:12:16 +0900

    [Fix]Rspecを修正

--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :episode do
     anime
     sequence(:num) { |n| n }
-    sequence(:subtitle) { |n| "サブタイトル#{n}" }
+    sequence(:subtitle) { |n| "test#{n}_subtitle" }
     sequence(:broadcast_datetime) { DateTime.now -1 }
     air_time { 30 }
     active { true }
--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     sequence(:tweet_id) { |n| n }
     sequence(:serial_number) { |n| n }
     sequence(:progress_time_msec) { serial_number * 1000 }
-    sequence(:name) { |n| "test_name#{n}" }
-    sequence(:screen_name) { |n| "test_screen_name#{n}" }
+    sequence(:name) { |n| "test#{n}_name" }
+    sequence(:screen_name) { |n| "test#{n}_screen_name" }
     sequence(:profile_image_url) { "https://placeimg.com/48/48/nature" }
-    sequence(:text) { |n| "test_text#{n}" }
+    sequence(:text) { |n| "test#{n}_text" }
     sequence(:tweeted_at) { |n| DateTime.now - n }
   end
 end
