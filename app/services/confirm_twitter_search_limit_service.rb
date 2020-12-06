999171d2acc4f1fcb6ab0b528e0060bcdf43ad0a
estsn122 <y.tani9411@gmail.com>
2020-12-06 19:09:11 +0900

    [Add]残りのツイート取得可能数を表示

---
+++ app/services/confirm_twitter_search_limit_service.rb
@@ -0,0 +1,16 @@
+class ConfirmTwitterSearchLimitService
+  private_class_method :new
+
+  def self.call
+    new.call
+  end
+
+  def call
+    twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Settings.dig(:twitter, :consumer_key)
+      config.consumer_secret     = Settings.dig(:twitter, :consumer_secret)
+    end
+    twitter_limit_list = Twitter::REST::Request.new(twitter, :get, '/1.1/application/rate_limit_status.json').perform[:resources]
+  twitter_limit_list[:search][:"/search/tweets"][:remaining]
+  end
+end
