e3bdffb2ad87f32157a2a8220b42615ae6d8a11a
estsn122 <y.tani9411@gmail.com>
2020-12-12 19:19:30 +0900

    [Fix]CircleCIがエラーとなるのを修正

--- spec/system/admin/animes/import_tweets_spec.rb
+++ spec/system/admin/animes/import_tweets_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'admin/animes/import_tweets', type: :system do
     let!(:anime) { create(:anime) }
     let!(:episode) { create(:episode, :with_tweets, anime_id: anime.id) }
     context '表示内容' do
+      before do
+        allow(ConfirmTwitterSearchLimitService).to receive(:call).and_return(450)
+      end
       it 'twitterで検索する文字列が正しく表示される' do
         visit admin_animes_import_tweet_path(episode.id)
         expect(page).to have_content(episode.decorate.search_twitter)
@@ -23,6 +26,7 @@ RSpec.describe 'admin/animes/import_tweets', type: :system do
     let!(:episode) { create(:episode, anime_id: anime.id) }
     context '正常処理' do
       before do
+        allow(ConfirmTwitterSearchLimitService).to receive(:call).and_return(450)
         allow(SearchTweetsService).to receive(:call).and_return('hoge')
         allow(Tweet).to receive(:convert_from_json).and_return(build_list(:tweet, 5, episode_id: episode.id))
       end
