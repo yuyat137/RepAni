b05c5ac7e972ba7d03a73de2dc77ab0852e6917b
estsn122 <y.tani9411@gmail.com>
2020-12-12 20:28:10 +0900

    [Fix]import_tweetsをtweets_importsに修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
       resources :episodes, only: %w[edit update], param: :anime_id
       resources :episodes, only: %w[destroy], param: :episode_id
       resources :tweets, only: %w[index]
-      resources 'import_tweets', only: %w[show], param: :episode_id
-      post 'import_tweets/import', to: 'import_tweets#import'
+      resources 'tweets_imports', only: %w[new create], param: :episode_id
     end
     resources :animes
     resources :episodes, only: %w[index]
