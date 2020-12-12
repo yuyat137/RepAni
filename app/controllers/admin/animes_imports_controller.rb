cc777ef29bc34f52783d7c0bc33058b946fbca37
estsn122 <y.tani9411@gmail.com>
2020-12-12 20:17:31 +0900

    [Fix]import_animesをanimes_importに変更

--- app/controllers/admin/animes_imports_controller.rb
+++ app/controllers/admin/animes_imports_controller.rb
@@ -1,19 +1,7 @@
-class Admin::ImportAnimesController < Admin::BaseController
-  def index; end
+class Admin::AnimesImportsController < Admin::BaseController
+  def new; end
 
   def create
-    result = Anime.register(params[:anime])
-
-    if result
-      flash.now[:success] = 'アニメを登録しました'
-    else
-      flash.now[:danger] = 'アニメの登録に失敗しました'
-    end
-
-    render :index
-  end
-
-  def import
     @animes = ImportAnimesFromApiService.call(params.dig(:import_period, 'year(1i)'), params.dig(:import_period, :season))
     if !@animes.blank?
       flash.now[:success] = 'ログ欄のアニメをインポートしました'
