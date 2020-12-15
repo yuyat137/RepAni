a36be532ae6dc52dc270e48198b71c61fb9a2c69
estsn122 <y.tani9411@gmail.com>
2020-12-15 21:20:41 +0900

    [Fix]termについては中間テーブルがあったりするため元のまま

--- app/controllers/admin/animes/terms_controller.rb
+++ app/controllers/admin/animes/terms_controller.rb
@@ -6,7 +6,11 @@ class Admin::Animes::TermsController < Admin::BaseController
   def update
     @anime = Anime.find(params[:anime_id])
     @anime.terms.destroy_all
+    params.dig(:anime, :terms_attributes)&.each do |_key, value|
+      next if ActiveRecord::Type::Boolean.new.cast(value[:_destroy])
 
+      @anime.register_term(value[:year], Term.seasons[value[:season]])
+    end
     redirect_to admin_anime_path(@anime), success: '放送時期を更新しました'
   end
 end
