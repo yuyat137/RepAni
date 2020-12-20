096cb1332f68ff99a90686bee57f18333dea9610
estsn122 <y.tani9411@gmail.com>
2020-12-20 20:17:05 +0900

    [Add]公開中のアニメ・エピソードしか表示されないことを確認するRspecを作成

--- spec/system/anime_spec.rb
+++ spec/system/anime_spec.rb
@@ -1,29 +1,40 @@
 require 'rails_helper'
 RSpec.describe 'Anime', type: :system do
-  let!(:anime1) { create(:anime, :with_now_term, :with_episodes) }
-  let!(:anime2) { create(:anime, :with_term, :with_episodes) }
-  context 'ページを開いた直後' do
+  let!(:anime_now) { create(:anime, :with_now_term, public: true) }
+  let!(:anime_not_now) { create(:anime, :with_term, public: true) }
+  let!(:anime_public) { create(:anime, :with_now_term, public: true) }
+  let!(:anime_private) { create(:anime, :with_now_term, public: false) }
+  context 'アニメ表示画面' do
     it '今期アニメ情報が画面に表示されている' do
       visit '/anime'
-      expect(page).to have_content(anime1.title)
-      expect(page).not_to have_content(anime2.title)
+      expect(page).to have_content(anime_now.title)
+      expect(page).not_to have_content(anime_not_now.title)
+    end
+    it '公開中のアニメのみ表示される' do
+      visit '/anime'
+      expect(page).to have_content(anime_public.title)
+      expect(page).not_to have_content(anime_private.title)
     end
   end
   context 'アニメ期間を選択' do
-    before { anime1.terms.first.update(now: true) }
     it '選択した期間のアニメ情報が画面に表示されている' do
       visit '/anime'
-      find("#term_#{anime2.terms.first.id}").click
-      expect(page).to have_content(anime2.title)
-      expect(page).not_to have_content(anime1.title)
+      find("#term_#{anime_not_now.terms.first.id}").click
+      expect(page).to have_content(anime_not_now.title)
+      expect(page).not_to have_content(anime_now.title)
     end
   end
-  context 'アニメを選択する' do
-    it 'エピソード選択画面が表示される' do
+  context 'エピソード表示ダイアログ' do
+    before do
+      create(:episode, anime_id: anime_public.id, num: 1, public: true)
+      create(:episode, anime_id: anime_public.id, num: 2, public: false)
+    end
+    it '公開中のエピソードのみ表示される' do
       visit '/anime'
-      find("#anime_#{anime1.id}").click
+      find("#anime_#{anime_public.id}").click
       expect(page).to have_content("サブタイトルを選んでください")
       expect(page).to have_link '1'
+      expect(page).not_to have_link '2'
     end
   end
 end
