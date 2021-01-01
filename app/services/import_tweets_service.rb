49b51a33d0265e63a01ba497907e7fc8ce6b260c
estsn122 <y.tani9411@gmail.com>
2021-01-01 11:12:45 +0900

    [Fix]変数名変更

--- app/services/import_tweets_service.rb
+++ app/services/import_tweets_service.rb
@@ -23,42 +23,41 @@ class ImportTweetsService
   # TODO: 原因不明だが、たまに画像を取得できないツイートがあることを確認済み
   def fetch_tweets_at_anime_broadcast(max_tweet_id)
     count = 0
-    fetch_tweets = []
+    new_tweets = []
 
     # ツイート取得
-    tweets = @twitter.search(@hashtag, max_id: max_tweet_id).attrs[:statuses]
+    fetch_tweets = @twitter.search(@hashtag, max_id: max_tweet_id).attrs[:statuses]
     count += 100
-    last_tweet = tweets.last
+    last_tweet = fetch_tweets.last
     # ツイートを時間で抽出
-    tweets = extract_tweets(tweets)
+    fetch_tweets = extract_tweets(fetch_tweets)
     # ツイートの型を変換
-    tweets = Tweet.convert_from_json(tweets, @episode.broadcast_datetime, @episode.id)
+    fetch_tweets = Tweet.convert_from_json(fetch_tweets, @episode.broadcast_datetime, @episode.id)
     # ツイートを結合
-    fetch_tweets.concat(tweets)
+    new_tweets.concat(fetch_tweets)
 
     while calculate_diff_tweeted_sec(last_tweet) > 0
       # ツイート取得
-      tweets = @twitter.search(@hashtag, max_id: last_tweet[:id] - 1).attrs[:statuses]
+      fetch_tweets = @twitter.search(@hashtag, max_id: last_tweet[:id] - 1).attrs[:statuses]
       count += 100
-      last_tweet = tweets.last
+      last_tweet = fetch_tweets.last
       # ツイートを時間で抽出
-      tweets = extract_tweets(tweets)
+      fetch_tweets = extract_tweets(fetch_tweets)
       # ツイートの型を変換
-      tweets = Tweet.convert_from_json(tweets, @episode.broadcast_datetime, @episode.id)
+      fetch_tweets = Tweet.convert_from_json(fetch_tweets, @episode.broadcast_datetime, @episode.id)
       # ツイートを結合
-      fetch_tweets.concat(tweets)
+      new_tweets.concat(fetch_tweets)
 
       if count > IMPORT_TWEETS_NUM
-        Tweet.import fetch_tweets
+        Tweet.import new_tweets
         count = 0
-        fetch_tweets = []
+        new_tweets = []
       end
     end
   end
 
   # ここのメソッド名後で変更
   def calculate_diff_tweeted_sec(tweet)
-    # ここにtweetが渡らないことがある
     # ここのto_sは後で変えたい
     Time.parse(tweet[:created_at].to_s) - Time.parse(@episode.broadcast_datetime.to_s)
   end
@@ -68,9 +67,6 @@ class ImportTweetsService
     start_time = @episode.broadcast_datetime
     extracted_tweets = []
     fetched_tweets.each do |tweet|
-      if tweet[:created_at].nil?
-        binding.pry
-      end
       extracted_tweets << tweet if start_time < DateTime.parse(tweet[:created_at])
     end
     extracted_tweets
