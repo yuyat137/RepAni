51f73b134416c44bb172475b86f7a679ca135144
estsn122 <y.tani9411@gmail.com>
2021-01-01 11:39:49 +0900

    [Add]仮のメソッドを作成

--- app/controllers/admin/animes/episodes/tweets_imports_controller.rb
+++ app/controllers/admin/animes/episodes/tweets_imports_controller.rb
@@ -8,4 +8,11 @@ class Admin::Animes::Episodes::TweetsImportsController < Admin::BaseController
     ImportTweetsService.call(@episode.id, params[:tweet_id])
     redirect_to admin_anime_episode_tweets_path(@episode.id), success: 'ツイートを取得しました'
   end
+
+  def more_import
+    @episode = Episode.find(params[:episode_id])
+    tweet_id = @episode.tweets.last.id
+    ImportTweetsService.call(@episode.id, tweet_id)
+    redirect_to admin_anime_episode_tweets_path(@episode.id), success: 'ツイートを取得しました'
+  end
 end
