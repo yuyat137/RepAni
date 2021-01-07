0ac0a68174564f0c9814b6bfb1b531d76da87fcf
estsn122 <y.tani9411@gmail.com>
2021-01-07 22:50:34 +0900

    [Fix]computed名を修正

--- app/javascript/pages/anime/components/AnimeList.vue
+++ app/javascript/pages/anime/components/AnimeList.vue
@@ -12,7 +12,7 @@
           </thead>
           <tbody>
             <tr
-              v-for="anime in sortedAnimes"
+              v-for="anime in sortedPublicAnimes"
               :key="anime.id"
               @click="handleSelectAnime(anime)"
               :class="changeColor(anime)"
@@ -46,7 +46,7 @@ export default {
         return anime.public == true
       })
     },
-    sortedAnimes () {
+    sortedPublicAnimes () {
       return this.publicAnimes.sort(function(a,b){
         if(a.title < b.title) return -1;
         if(a.title > b.title) return 1;
