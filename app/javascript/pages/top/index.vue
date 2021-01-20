9519065902794e21ffeaaeed1886aeb5db123d9c
estsn122 <y.tani9411@gmail.com>
2021-01-20 21:10:23 +0900

    [Add]ボックス仮実装

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -17,36 +17,16 @@
             :to="{ name: 'AnimeIndex' }"
           >
             <v-card
-              height="20%"
-              class="headline my-5 d-flex align-center"
+              class="headline d-flex align-center"
               color="#70bee6"
+              height="25%"
               dark
             >
               <p class="mx-auto my-0">
-                放送時期から探す
+                アニメを探す
               </p>
             </v-card>
           </router-link>
-          <v-card
-            height="20%"
-            class="headline my-5 d-flex align-center"
-            color="#0174DF"
-            dark
-          >
-            <p class="mx-auto my-0">
-              未実装
-            </p>
-          </v-card>
-          <v-card
-            height="20%"
-            class="headline my-5 d-flex align-center"
-            color="#52cc7a"
-            dark
-          >
-            <p class="mx-auto my-0">
-              未実装
-            </p>
-          </v-card>
         </v-col>
       </v-row>
     </v-container>
