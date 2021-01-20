1a2cc80185367a35be7684ead73101b79fbb650e
estsn122 <y.tani9411@gmail.com>
2021-01-20 21:18:59 +0900

    [Add]ボックスを仮実装

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -13,20 +13,22 @@
         <v-col
           cols="4"
         >
-          <router-link
-            :to="{ name: 'AnimeIndex' }"
-          >
-            <v-card
-              class="headline d-flex align-center"
-              color="#70bee6"
-              height="25%"
-              dark
+          <div id="to_anime_list">
+            <router-link
+              :to="{ name: 'AnimeIndex' }"
             >
-              <p class="mx-auto my-0">
-                アニメを探す
-              </p>
-            </v-card>
-          </router-link>
+              <v-card
+                class="headline d-flex align-center"
+                color="#70bee6"
+                height="25%"
+                dark
+              >
+                <p class="mx-auto my-0">
+                  アニメを探す
+                </p>
+              </v-card>
+            </router-link>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -38,3 +40,10 @@ export default {
   name: "TopIndex",
 }
 </script>
+
+<style scoped>
+#to_anime_list {
+  margin-top: 200px;
+  height: 80%;
+}
+</style>
